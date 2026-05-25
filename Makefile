134df45991ece6ddd6556bc78bf99cd2c2f7d6f5
Mariana Sena <mariana7007@hotmail.com>
2026-05-25 19:39:52 -0300

    memcpy

--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ SRC = ft_isalpha.c \
       ft_isprint.c \
       ft_strlen.c  \
 	  ft_memset.c  \
-	  ft_bzero.c
+	  ft_bzero.c   \
+	  ft_memcpy.c
 
 OBJ = $(SRC:.c=.o)
 
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/14 18:16:53 by mariafer          #+#    #+#             */
-/*   Updated: 2026/05/14 18:16:56 by mariafer         ###   ########.fr       */
+/*   Created: 2026/05/25 18:03:27 by mariafer          #+#    #+#             */
+/*   Updated: 2026/05/25 18:03:29 by mariafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t	i;
+	char	*d;
+	char	*s;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-void	*ft_memset(void *s, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-
-#endif
+	i = 0;
+	d = (char *)dst;
+	s = (char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
+}
