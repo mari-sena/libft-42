b04a8e4e19dfb75a84dfd0112b68178121cb0c7f
Mariana Sena <mariana7007@hotmail.com>
2026-05-26 17:49:27 -0300

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 11:29:56 by mariafer          #+#    #+#             */
+/*   Updated: 2026/05/26 11:29:58 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0 ;
+	return (ft_strlen(src));
+}
