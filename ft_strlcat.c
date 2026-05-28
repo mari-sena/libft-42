adc3cd2b5c6799df3f49ab93fc0b75bffc11bd14
Mariana Sena <mariana7007@hotmail.com>
2026-05-28 09:46:59 -0300

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 17:50:10 by mariafer          #+#    #+#             */
+/*   Updated: 2026/05/26 17:50:11 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_dst_len(char *dst, size_t size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (dst[i] != '\0' && i < size)
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	unsigned int	i;
+	unsigned int	j;
+	size_t			src_size;
+
+	j = 0;
+	i = ft_dst_len(dst, dstsize);
+	src_size = ft_strlen(src);
+	if (i < dstsize)
+	{
+		while (i + j < dstsize - 1 && src[j] != '\0')
+		{
+			dst[i + j] = src[j];
+			j++;
+		}
+		dst[i + j] = '\0';
+	}
+	return (i + src_size);
+}
--- libft.h
+++ libft.h
@@ -20,9 +20,16 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
-size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(
+			char *restrict dst,
+			const char *restrict src,
+			size_t dstsize);
+size_t	ft_strlcat(
+			char *restrict dst,
+			const char *restrict src,
+			size_t dstsize);
 
 #endif
