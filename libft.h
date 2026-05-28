6172e391cb393fa3b33382d6193585472dfd688f
Mariana Sena <mariana7007@hotmail.com>
2026-05-28 10:54:04 -0300

    strchr

--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t n);
