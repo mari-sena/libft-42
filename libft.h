c0b6c40a175e120e2de6df229028147c6b6b775e
Mariana Fernandes Guimaraes <mariafer@c1r2p2.42sp.org.br>
2026-06-01 11:57:48 -0300

    atoi in construction

--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *nptr);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	ft_bzero(void *s, size_t n);
