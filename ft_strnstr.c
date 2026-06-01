20422f012a98e6cbace03f293a6349723a23d624
Mariana Sena <mariana7007@hotmail.com>
2026-06-01 13:19:33 -0300

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,16 +18,16 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	while (haystack[i] && i < len)
+	if (!needle[0])
+		return ((char *)haystack);
+	while (i < len && haystack[i])
 	{
 		j = 0;
 		while (needle[j]
-			&& haystack[i + j]
 			&& i + j < len
+			&& haystack[i + j]
 			&& haystack[i + j] == needle[j])
-		{
 			j++;
-		}
 		if (!needle[j])
 			return ((char *)haystack + i);
 		i++;
