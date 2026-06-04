300ac7fa3524b18323998576c048e359c0b0b0a1
Mariana Fernandes Guimaraes <mariafer@c3r9p5.42sp.org.br>
2026-06-04 12:54:11 -0300

    norm fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,5 +14,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	
+	char	*new;
+
+	new = malloc(size * count);
+	if (!new)
+		return (NULL);
+	ft_memset(new, 0, count * size);
+	return (new);
 }
