f08fe7ee6dab2b9c93bb5ce2bd50edb6424f510f
Mariana Sena <mariana7007@hotmail.com>
2026-06-05 14:51:57 -0300

    ft putnbr

--- libft.h
+++ libft.h
@@ -33,6 +33,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
