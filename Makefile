37e562c4633aa7aadf9cebe9ee8926bba9c9e1dc
Mariana Sena <mariana7007@hotmail.com>
2026-06-05 22:55:44 -0300

    çstadd_back

--- Makefile
+++ Makefile
@@ -37,7 +37,8 @@ SRC = ft_isalpha.c		\
 	  ft_lstnew.c		\
 	  ft_lstadd_front.c	\
 	  ft_lstsize.c		\
-	  ft_lstlast.c
+	  ft_lstlast.c		\
+	  ft_lstadd_back.c
 
 OBJ = $(SRC:.c=.o)
 
---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/05 22:40:32 by mariafer          #+#    #+#             */
+/*   Updated: 2026/06/05 22:40:34 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
