1321ac9f811ceb6cf0092b0d85db57e3f58bf88c
Mariana Sena <mariana7007@hotmail.com>
2026-06-06 12:32:07 -0300

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -19,11 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if ((unsigned char)s[i] == (unsigned char)c)
 			return ((char *)s + i);
 		i++;
 	}
-	if (!c && s[i] == '\0')
+	if ((unsigned char)s[i] == (unsigned char)c)
 		return ((char *)s + i);
 	return (NULL);
 }
