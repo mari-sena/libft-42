0101a4db11c9d23463689b69d1b56663d82f28bb
Mariana Sena <mariana7007@hotmail.com>
2026-06-06 11:52:49 -0300

    strampi fix segment fault

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -18,6 +18,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 
 	i = 0;
+	if (!s || !f)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
 	if (str == NULL)
 		return (NULL);
