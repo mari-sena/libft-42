8bdf57e8fb45598d19019a9ce22188f8e71d65d8
Mariana Sena <mariana7007@hotmail.com>
2026-06-06 12:28:57 -0300

    fix strrchr name

--- libft.h
+++ libft.h
@@ -30,7 +30,7 @@ int		ft_atoi(const char *nptr);
 char	*ft_itoa(int n);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
-char	*strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
