aaa2a66e7d906554015a0a1f36676d67f2450e0d
Mariana Sena <mariana7007@hotmail.com>
2026-06-06 11:13:06 -0300

    fix split

--- libft.h
+++ libft.h
@@ -27,7 +27,7 @@ int		ft_atoi(const char *nptr);
 char	*ft_itoa(int n);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
-// char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
